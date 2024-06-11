5e36bde54e45926c486701987e6ed9d878189158
Eric Durocher <erciduro@gmail.com>
2024-06-10 20:40:57 -0400

    finished project

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import ContextProvider from "@/app/providers/ContextProvider";
 import Sidebar from "@/app/Components/Sidebar/Sidebar";
 
 import "./globals.css";
+import NextTopLoader from "nextjs-toploader";
 
 const nunito = Nunito({
     weight: ["400", "500", "600", "700", "800"],
@@ -35,6 +36,11 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
                     />
                 </head>
                 <body className={ nunito.className }>
+                    <NextTopLoader
+                        height={2}
+                        color="#27AE60"
+                        easing="cubic-bezier(0.53,0.21,0,1)"
+                    />
                     <ContextProvider>
                         <GlobalStyleProvider>
                             {userId && <Sidebar />}
